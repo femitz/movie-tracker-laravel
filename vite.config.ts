2f395da0fc5b272eab339605468ca247dfc5e7e5
Felipe Schmitz <schmitzdz14@gmail.com>
2025-08-26 22:07:46 -0300

    feat: atualiza configurações do ambiente, ajusta scripts de build e melhora a configuração do Vite para uso com ngrok

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
+const isNgrok = process.env.VITE_APP_URL?.includes('ngrok');
+
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.ts'],
+            input: ['resources/css/app.css', 'resources/js/app.ts'], // caminhos corretos
             ssr: 'resources/js/ssr.ts',
             refresh: true,
         }),
@@ -20,4 +22,17 @@ export default defineConfig({
             },
         }),
     ],
+    base: '',       // caminhos relativos, evita mixed content
+    server: {
+        host: true,
+        port: 5173,
+        strictPort: true,
+        hmr: isNgrok
+        ? {
+            protocol: 'wss',
+            host: process.env.VITE_APP_URL?.replace(/^https?:\/\//, ''),
+            clientPort: 443,
+          }
+        : true, // dev local
+    },
 });
